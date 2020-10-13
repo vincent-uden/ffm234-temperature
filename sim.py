49bf0a4ea0209f74a20a9e92389eb1145f2d896c
Vincent Uden <vincent.uden@itggot.se>
2020-10-13 11:46:02 +0200

    Added first files

---
+++ sim.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+from math import pow
+
+RHO = 1500.0
+LAMBDA = 1.0
+C = 1000.0
+K = 0.25
+X_SIZE = 30
+k = LAMBDA / (C*RHO)
+DELTA_T = K * pow((1/float(X_SIZE)),2) / k
+
+x = np.linspace(0, 1, num=X_SIZE)
+T = np.zeros(X_SIZE)
+T[0] = -10.0
+
+def step_time(T, K):
+    output = T + K * (np.roll(T,-1) - 2*T + np.roll(T,1))
+    output[0] = -10.0
+    output[-1] = 0.0
+    return output
+
+all_T = [T]
+while True:
+    new_T = step_time(T, K)
+    if abs(np.mean(new_T - T)) < 0.0001:
+        break
+    T = new_T
+    all_T.append(T)
+
+all_T = np.array(all_T).T
+
+plt.imshow(all_T, aspect="auto", cmap="coolwarm")
+plt.colorbar()
+plt.show()
+
+
